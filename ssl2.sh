2bd6c3b9f413f6baa56c5d858bda69171c31f06d
Health Ventures <paul@healthventures.info>
2018-12-18 09:45:41 -0500

    added prompt

--- ssl2.sh
+++ ssl2.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 echo "...Get SSL Certificate for Domain"
-sudo ./certbot-auto certonly --webroot -w /opt/bitnami/apps/wordpress/htdocs/ -d www.lindaarnold.org -d lindaarnold.org
+
+read -p "What is your baseline domain name? :> " domainname
+echo ""
+
+wwwdomainname="www.$domainname"
+
+echo "We will proceed to install the certificate with:"
+echo "$domainname and $wwwdomainname"
+
+read -p "Continue (y/n)? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+
+# CONTINUE TO INSTALL CERT
+
+sudo ./certbot-auto certonly --webroot -w /opt/bitnami/apps/wordpress/htdocs/ -d $wwwdomainname -d $domainname
 echo "...Rename existing keys to .old"
 mv /opt/bitnami/apache2/conf/server.key /opt/bitnami/apache2/conf/serverkey.old
 mv /opt/bitnami/apache2/conf/server.crt /opt/bitnami/apache2/conf/servercrt.old
 echo "...Create symlink to Domain SSL keys"
-sudo ln -s/etc/letsencrypt/live/www.lindaarnold.org/fullchain.pem /opt/bitnami/apache2/conf/server.crt
-sudo ln -s/etc/letsencrypt/live/lindaarnold.org/privkey.pem /opt/bitnami/apache2/conf/server.key
+sudo ln -s/etc/letsencrypt/live/$wwwdomainname/fullchain.pem /opt/bitnami/apache2/conf/server.crt
+sudo ln -s/etc/letsencrypt/live/$wwwdomainname/privkey.pem /opt/bitnami/apache2/conf/server.key
 echo "...Next Step: Update wp_config for https prefix"
 echo "...Next Step: Restart Apache"
