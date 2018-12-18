2883606800fea07c7fee79522bc560f6e47d9cee
Health Ventures <paul@healthventures.info>
2018-12-18 10:24:30 -0500

    set executable

--- promptdomainname.sh
+++ promptdomainname.sh
@@ -11,6 +11,15 @@ wwwdomainname="www.$domainname"
 
 echo "Your domain with the www prefix is > $wwwdomainname"
 
+# --- figure out how to pause better
+read -p "Continue (y/n)? " -n 1 -r
+if 
+	[[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	exit 1
+
+eco "script continues because of yes"	
+
  
 
 
