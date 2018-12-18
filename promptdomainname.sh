44bc4fdca682ccc6e7b5b17990aa5634a203b391
Health Ventures <paul@healthventures.info>
2018-12-18 09:04:19 -0500

    prompting code

---
+++ promptdomainname.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get user input test script
+
+read -p "What is your baseline domain name :> " domainname
+echo ""
+echo "You entered $domainname"
+echo ""
+
+wwwdomainname="www.$domainname"
+
+echo "Your domain with the www prefix is > $wwwdomainname"
+ 
+
+
+
