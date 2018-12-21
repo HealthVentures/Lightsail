35642cd9b67be35be4e55772c5402bdad6ec8095
Paul Helmick <paul@healthventures.info>
2018-12-20 19:45:37 -0500

    multisite

---
+++ prep_multisite.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# LIGHTSAIL MULTISITE PREP
+#
+# DOCS: 
+# https://docs.bitnami.com/aws/apps/wordpress-multisite/
+# https://docs.bitnami.com/aws/apps/wordpress-multisite/configuration/configure-wordpress-multisite/
+#
+echo "]]] Lightsail Multisite Prep [[["
+echo ""
+
+echo "... Turn of Bitnami Banner"
+
+sudo /opt/bitnami/apps/wordpress/bnconfig — disable_banner 1
+
+
+echo ""
+read -p "What is your baseline domain name? :> " domainname
+echo ""
+
+# wwwdomainname="www.$domainname"
+
+echo "... Update Bitnami domain name"
+sudo ./bnconfig — machine_hostname $domainname
+
+echo "... Turn off Bitnami auto IP reset on reboot"
+sudo mv bnconfig bnconfig.disabled
+
+echo "... Restarting Apache"
+sudo /opt/bitnami/ctlscript.sh restart apache
+
+echo "... Next install SSL"
+
+# multiblog ssl doc
+# https://docs.bitnami.com/aws/apps/wordpress-multisite/administration/use-different-ssl-certificates/
+
+echo "... Also update wp_config domain names"
