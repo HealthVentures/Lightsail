9bf9efbac00f7cd2fbd844ef54d6ff619e292079
Health Ventures <paul@healthventures.info>
2019-01-14 14:15:15 -0500

    updates

--- wpclean.sh
+++ wpclean.sh
@@ -5,7 +5,11 @@
 cd /opt/bitnami/apps/wordpress/htdocs/
 
 # remove files
+echo "]]] REMOVING OLD WORDPRESS FILES..."
 sudo rm -rf wp*
 rm xmlrpc.php 
 sudo rm -rf readm*
 sudo rm -f license.txt index.php .ht*
+clear
+echo "]]] WORDPRESS CLEANED"
+ls -salt /opt/bitnami/apps/wordpress/htdocs/
