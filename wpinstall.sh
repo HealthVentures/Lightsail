2bc814483067dd088ac5d60ddd5031ee3590c7ca
Paul Helmick <paul@healthventures.info>
2019-12-06 12:34:33 -0500

    updates

--- wpinstall.sh
+++ wpinstall.sh
@@ -25,22 +25,23 @@ echo "]]] SET PERMISSIONS..."
 
 cd /opt/bitnami/apps/wordpress/htdocs/
 
+sudo chown -R bitnami:daemon *
+
 sudo find . -type d -exec chmod 0755 {} \;
 sudo find . -type f -exec chmod 0644 {} \;
 
-echo "]]] SET OWNERSHIP..."
-
-sudo chown -R bitnami:daemon /opt/bitnami/apps/wordpress/htdocs/wp-content
-sudo chmod -R g+w /opt/bitnami/apps/wordpress/htdocs/wp-content
+# sudo chmod -R g+w /opt/bitnami/apps/wordpress/htdocs/wp-content
 
 echo "]]] SECURE WPCONFIG..."
 
 sudo chmod 640 wp-config.php
 
 echo "]]] WORDPRESS INSTALLED..."
+
 ls -salth /opt/bitnami/apps/wordpress/htdocs
 
 echo "]]] RESTART SERVICES ..."
+
 sudo /opt/bitnami/ctlscript.sh restart
 
 echo "]]] DONE ..."
--- wprestore.sh
+++ wprestore.sh
@@ -7,7 +7,7 @@ clear
 cd /opt/bitnami/apps/wordpress/htdocs/
 
 echo "]]] RESTORE DIVI THEME..."
-cp -fr sav/themes/Divi wp-content/themes/
+cp -fr sav/themes/ wp-content/
 
 echo "]]] RESTORE PLUGINS..."
 cp -fr sav/plugins/ wp-content/
@@ -23,13 +23,12 @@ echo "]]] SET PERMISSIONS..."
 
 cd /opt/bitnami/apps/wordpress/htdocs/
 
+sudo chown -R bitnami:daemon /opt/bitnami/apps/wordpress/htdocs/wp-content
+
 sudo find . -type d -exec chmod 0755 {} \;
 sudo find . -type f -exec chmod 0644 {} \;
 
-echo "]]] SET OWNERSHIP..."
-
-sudo chown -R bitnami:daemon /opt/bitnami/apps/wordpress/htdocs/wp-content
-sudo chmod -R g+w /opt/bitnami/apps/wordpress/htdocs/wp-content
+# sudo chmod -R g+w /opt/bitnami/apps/wordpress/htdocs/wp-content
 
 echo "]]] SECURE WPCONFIG..."
 
