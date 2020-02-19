c8fe77820e004e557da48984533e6e3de4d7c596
Health Ventures <paul@healthventures.info>
2020-02-19 15:05:33 -0500

    update permissions to 775

--- wpown.sh
+++ wpown.sh
@@ -10,7 +10,7 @@ cd /opt/bitnami/apps/wordpress/htdocs/
 
 sudo chown -R bitnami:daemon *
 
-sudo find . -type d -exec chmod 0755 {} \;
+sudo find . -type d -exec chmod 0775 {} \;
 sudo find . -type f -exec chmod 0644 {} \;
 
 sudo chmod -R g+w /opt/bitnami/apps/wordpress/htdocs/wp-content
--- wprestore.sh
+++ wprestore.sh
@@ -25,7 +25,7 @@ cd /opt/bitnami/apps/wordpress/htdocs/
 
 sudo chown -R bitnami:daemon /opt/bitnami/apps/wordpress/htdocs/wp-content
 
-sudo find . -type d -exec chmod 0755 {} \;
+sudo find . -type d -exec chmod 0775 {} \;
 sudo find . -type f -exec chmod 0644 {} \;
 
 sudo chmod -R g+w /opt/bitnami/apps/wordpress/htdocs/wp-content
