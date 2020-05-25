17ec93c480f726dbfa47e2234ce8aa59435302c7
Health Ventures <paul@healthventures.info>
2020-05-25 19:26:53 -0400

    update log in clearcache

--- clearcache.sh
+++ clearcache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-cd /opt/bitnami/apps/wordpress/htdocs/wp-content
+cd /opt/bitnami/apps/wordpress/htdocs/
+sudo du -shc wp-content/* | grep et-cache
+cd wp-content
 sudo rm -rf et-cache
 cd ~
-ls
-
